35a753754c2f11f4d9c73a426424f8bf1edaf02a
garrick george <garrickgeorge@garricks-Air.Home>
2017-03-07 19:46:17 -0700

    update

--- gd.js
+++ gd.js
@@ -1,24 +1,73 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var food = $('tfoot').children();
+    var tbody = $('tbody');
+    var tamt = 0;
+    var xnum = $('tfoot td:contains(tamt)');
 
-    var food = $('th');
-    var icy = 'Ice Cream Biscut, 7.99';
-    var burger = 'Royale with Cheese, 8.99';
-    var pizza = 'Arugula Pie, 11.99';
-    var bbq = 'Smoked Swine, 14.99';
 
-    $('#icy').on('click', function() {
-        console.log('its on!');
-        var $ntr = $('<tr>');
-        var $tdn = $('<td data-field="name">Ice Cream Biscuit</td>');
-        var $tdp = $('<td data-field="price">7.99</td>');
-        food.append($tdn, $tdp);
-        // food.append($tdp);
-        // food.append($tdn);
+    $('#bbq').on('click', function() {
+        var $tr = $('<tr class="item">');
+      tbody.append($tr);
+
+      var $tdn = $('<td data-field="name">Smoked Swine</td>');
+      var $tdp = $('<td data-field="price">14.99</td><br>');
+      var amt = 15.74;
+
+      $tr.append($tdn);
+      $tr.append($tdp);
+      tamt += amt;
+      $('#bus').html(tamt);
     })
-    $('td').on('click', '.card-action', function(event) {
-        var $target = $(event.target);
-        $target.remove();
-        console.log(`nope', ${$target.text()}!`);
-    });
+  $('#pizza').on('click', function() {
+        var $tr = $('<tr class="item">');
+    tbody.append($tr);
+        var amt = 12.59;
 
-})
+        var $tdn = $('<td data-field="name">Arugula Pie</td><br>');
+        var $tdp = $('<td data-field="price">11.99</td><br>');
+
+    $tr.append($tdn);
+    $tr.append($tdp);
+    tamt += amt;
+    $('#bus').html(tamt);
+  })
+  $('#burger').on('click', function() {
+
+        var $tr = $('<tr class="item">');
+
+    tbody.append($tr);
+        var amt = 9.44;
+        var $tdn = $('<td data-field="name">Royale Burger</td><br>');
+        var $tdp = $('<td data-field="price">8.99</td><brs>');
+    $tr.append($tdn);
+    $tr.append($tdp);
+    tamt += amt;
+    $('#bus').html(tamt);
+  });
+  $('#icy').on('click', function() {
+
+      var $tr = $('<tr class="item">');
+      var amt = 8.39;
+    tbody.append($tr);
+
+        var $tdn = $('<td data-field="name">Ice Cream Biscuit</td><br>');
+        var $tdp = $('<td data-field="price">7.99</td><br>');
+    $tr.append($tdn);
+    $tr.append($tdp);
+    tamt += amt;
+    $('#bus').html(tamt);
+  });
+
+  $('tbody').on('click', '.item', function(event) {
+        var $target = $(event.target);
+    if ($target.parent().hasClass('item')) {
+
+      let amt = $(event.target).parent('td:last-child').context.innerText;
+
+      amt = Number(amt * 1.05);
+      tamt -= amt;
+
+      $target.parent().remove();
+    }
+  });
+});
